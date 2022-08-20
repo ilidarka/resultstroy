db799e5c0e60a080829b971992545eb8903023a1
Vladi Leshko <leshkovladik@gmail.com>
2022-08-20 12:49:58 +0300

    fixed some issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,27 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";
 }
+
+//modal window script
+var modal = document.getElementById("modalWindow");
+var topBtn = document.getElementById("topPhoneButton");
+var bottomBtn = document.getElementById("bottomPhoneButton");
+var span = document.getElementsByClassName("closeButton")[0];
+
+topBtn.onclick = function() {
+    modal.style.display = "block";
+};
+
+bottomBtn.onclick = function() {
+    modal.style.display = "block";
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+};
